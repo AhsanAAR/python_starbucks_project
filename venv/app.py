73c2db35ce40ccc4b2a1681ee2701386c4a4550e
ali2faraz <63406806+ali2faraz@users.noreply.github.com>
2020-04-11 03:03:27 +0500

    done with creation and taking login input

--- venv/app.py
+++ venv/app.py
@@ -1,29 +1,19 @@
 from tkinter import *
 import tkinter.messagebox
+from functools import partial
 
-login = Tk()
-loginf=Frame(login,width=240,height=240)
-loginf.pack()
-L_UName=Label(loginf, text="Username ",bg="red", fg="white")
-L_Pass=Label(loginf, text="Password ",bg="red", fg="white")
-L_Img=PhotoImage(file="unnamed.png")
-L_login=Label(loginf, image=L_Img)
-#L_login.place(x=0, y=0, relwidth=1, relheight=1)
-E_UName= Entry(loginf)
-E_Pass= Entry(loginf)
-def gob():
-    return
-def Login(event):
+def Login(username, password):
     i=int(0)
     found=bool(False)
-    d_uname = "Mariam"
-    d_pass = "Saqer"
+    d_uname = username.get()
+    d_pass = password.get()
     d_name=""
     d_address=""
     d_tel=""
     d_email=""
     d_snum=""
     d_credit=""
+    d_type=""
     with open("WebUser.txt","r") as f:
         for line in f:
             if(line.find(d_uname)!=-1 and line.find(d_pass)!=-1):
@@ -37,14 +27,15 @@ def Login(event):
         with open("Records.txt", "r") as f:
             for line in f:
                 line = line.split('!')
-                d_uname = line[0]
-                d_pass = line[1]
-                d_name = line[2]
-                d_address = line[3]
-                d_tel = line[4]
-                d_email = line[5]
-                d_snum = line[6]
-                d_credit = line[7]
+                if line[0]==d_uname:
+                    d_pass = line[1]
+                    d_name = line[2]
+                    d_address = line[3]
+                    d_tel = line[4]
+                    d_email = line[5]
+                    d_snum = line[6]
+                    d_credit = line[7]
+                    d_type=line[8]
         success = Tk()
         s_uname = Label(success, text="User Name : " + d_uname)
         s_pass = Label(success, text="Password : " + d_pass)
@@ -54,78 +45,118 @@ def Login(event):
         s_email = Label(success, text="E-Mail : "+d_email)
         s_snum = Label(success, text="StarCard : "+d_snum)
         s_credit = Label(success, text="Credit : "+d_credit)
+        s_type = Label(success, text="Type : " + d_type)
         ok_Button = Button(success, text="Close",command=exit)
-
         s_name.grid(row=2,column=5,sticky=N)
         s_address.grid(row=3, column=5,sticky=N)
         s_tel.grid(row=4, column=5,sticky=N)
         s_email.grid(row=5, column=5,sticky=N)
         s_snum.grid(row=6, column=5,sticky=N)
         s_credit.grid(row=7, column=5,sticky=N)
-        s_uname.grid(row=8, column=5,sticky=N)
-        s_pass.grid(row=9, column=5,sticky=N)
+        s_type.grid(row=8, column=5, sticky=N)
+        s_uname.grid(row=9, column=5,sticky=N)
+        s_pass.grid(row=10, column=5,sticky=N)
         ok_Button.grid(row=11,column=8,sticky=N)
         success.geometry("500x500")
-def CreateUser(event):
+
+def getnewuserline(username, password,d_name,d_address,d_tel,d_email,d_snum,d_credit,d_type):
+    s_name =d_name.get()
+    s_address = d_address.get()
+    s_tel = d_tel.get()
+    s_email = d_email.get()
+    s_snum = d_snum.get()
+    s_credit = d_credit.get()
+    s_type=d_type.get()
+    s_uname=username.get()
+    s_pass=password.get()
+    line=s_uname+'!'+s_pass+'!'+s_name+'!'+s_address+'!'+s_tel+'!'+s_email+'!'+s_snum+'!'+s_credit+'!'+s_type+'!'
+    with open("WebUser.txt", "a") as f:
+        f.write(line)
+    tkinter.messagebox.showinfo("Creation", "User Created Successfully!")
+
+def CreateUser(username,password):
     i=int(0)
     found=bool(False)
-    d_uname = "Mariam"
-    d_pass = "Saqer"
-    d_name=""
-    d_address=""
-    d_tel=""
-    d_email=""
-    d_snum=""
-    d_credit=""
+    d_uname = username.get()
+    d_pass = password.get()
     with open("WebUser.txt","r") as f:
         for line in f:
-            if(line.find(d_uname)!=-1 and line.find(d_pass)!=-1):
+            if(line.find(d_uname)==-1):
                 found=True
             if(found):
                 break
     if(not found):
-        tkinter.messagebox.showinfo("Login Failed", "Username or Password Does not Match")
+        tkinter.messagebox.showinfo("UserName Taken", "Username Already Exist in database")
     else:
-        tkinter.messagebox.showinfo("Login", "Login SuccessFull")
-        with open("Records.txt", "r") as f:
-            for line in f:
-                line = line.split('!')
-                d_uname = line[0]
-                d_pass = line[1]
-                d_name = line[2]
-                d_address = line[3]
-                d_tel = line[4]
-                d_email = line[5]
-                d_snum = line[6]
-                d_credit = line[7]
+        tkinter.messagebox.showinfo("Creation", "Username is available!")
         success = Tk()
-        s_uname = Label(success, text="User Name : " + d_uname)
-        s_pass = Label(success, text="Password : " + d_pass)
-        s_name = Label(success, text="Name : " + d_name)
-        s_address = Label(success, text="Address : "+d_address)
-        s_tel = Label(success, text="Tel : "+d_tel)
-        s_email = Label(success, text="E-Mail : "+d_email)
-        s_snum = Label(success, text="StarCard : "+d_snum)
-        s_credit = Label(success, text="Credit : "+d_credit)
-        ok_Button = Button(success, text="Close",command=exit)
+        d_name = StringVar()
+        d_address = StringVar()
+        d_tel = StringVar()
+        d_email = StringVar()
+        d_snum = StringVar()
+        d_credit = StringVar()
+        d_type = StringVar()
+        createu = partial(getnewuserline, username, password,d_name,d_address,d_tel,d_email,d_snum,d_credit,d_type)
+        s_name = Label(success, text="Name : " )
+        s_address = Label(success, text="Address : ")
+        s_tel = Label(success, text="Tel : ")
+        s_email = Label(success, text="E-Mail : ")
+        s_snum = Label(success, text="StarCard : ")
+        s_credit = Label(success, text="Credit : ")
+        s_type = Label(success, text="Type : ")
+        s_uname=Label(success, text="UserName= "+d_uname)
+        s_pass = Label(success, text="Password= " + d_pass)
+        e_name = Entry(success, textvariable=d_name)
+        e_address = Entry(success, textvariable=d_address)
+        e_tel = Entry(success, textvariable=d_tel)
+        e_email = Entry(success, textvariable=d_email)
+        e_snum = Entry(success, textvariable=d_snum)
+        e_credit = Entry(success, textvariable=d_credit)
+        e_type = Entry(success, textvariable=d_type)
+        ok_Button = Button(success, text="Ok",command=createu)
 
-        s_name.grid(row=2,column=5)
-        s_address.grid(row=3, column=5)
-        s_tel.grid(row=4, column=5)
-        s_email.grid(row=5, column=5)
-        s_snum.grid(row=6, column=5)
-        s_credit.grid(row=7, column=5)
-        s_uname.grid(row=8, column=5)
-        s_pass.grid(row=9, column=5)
+        s_name.grid(row=2)
+        s_address.grid(row=3)
+        s_tel.grid(row=4)
+        s_email.grid(row=5)
+        s_snum.grid(row=6)
+        s_credit.grid(row=7)
+        s_type.grid(row=8)
+        e_name.grid(row=2,column=2)
+        e_address.grid(row=3,column=2)
+        e_tel.grid(row=4,column=2)
+        e_email.grid(row=5,column=2)
+        e_snum.grid(row=6,column=2)
+        e_credit.grid(row=7,column=2)
+        e_type.grid(row=8,column=2)
+        s_uname.grid(row=9)
+        s_pass.grid(row=10)
         ok_Button.grid(row=11,column=8)
         success.geometry("500x500")
 
 
+login = Tk()
+loginf=Frame(login,width=240,height=240)
+loginf.pack()
+username = StringVar()
+password = StringVar()
+validatelogin = partial(Login, username, password)
+create = partial(CreateUser, username, password)
+L_UName=Label(loginf, text="Username ",bg="red", fg="white")
+L_Pass=Label(loginf, text="Password ",bg="red", fg="white")
+L_Img=PhotoImage(file="unnamed.png")
+L_login=Label(loginf, image=L_Img)
+#L_login.place(x=0, y=0, relwidth=1, relheight=1)
+E_UName= Entry(loginf,textvariable=username)
+E_Pass= Entry(loginf,textvariable=password,show="*")
+def gob():
+    return
+
+
 Q_Button = Button(loginf, text="Quit", fg="red", command=loginf.quit)
-L_Button=Button(loginf, text="Login", fg="blue",)
-L_Button.bind("<Button-1>",Login)
-C_Button=Button(loginf, text="Create Account", fg="green",)
-C_Button.bind("<Button-1>",CreateUser)
+L_Button=Button(loginf, text="Login", fg="blue",command=validatelogin)
+C_Button=Button(loginf, text="Create Account", fg="green",command=create)
 L_login.grid(row=0,column=1)
 L_UName.grid(row=1)
 L_Pass.grid(row=2)
@@ -133,5 +164,6 @@ E_UName.grid(row=1, column=1)
 E_Pass.grid(row=2, column=1)
 Q_Button.grid(row=4)
 L_Button.grid(row=4, column=4)
+C_Button.grid(row=4, column=2)
 login.geometry("480x480")
 login.mainloop()
