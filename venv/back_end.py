b1ffb28bf8ca6048735eeab477e61587d58fa13e
AhsanAAR <39828020+AhsanAAR@users.noreply.github.com>
2020-04-11 17:35:10 +0500

    backend work

--- venv/back_end.py
+++ venv/back_end.py
@@ -1,11 +1,10 @@
-import app
-
 class star_card:
-    def __init__(self, cardNum, ):
-
+    def __init__(self, cardNum, credit):
+        self.m_cardNum = cardNum
+        self.m_credit = credit
 
 class web_user:
-    def __init__(self, uName, password, fullName, address, telNum, email, starCard):
+    def __init__(self, uName, password, fullName, address, telNum, email, starCard, accessList):
         self.m_uName = uName
         self.m_password = password
         self.m_fullName = fullName
@@ -13,3 +12,20 @@ class web_user:
         self.m_telNum = telNum
         self.m_email = email
         self.m_starCard = starCard
+        self.m_accessList = accessList
+
+class member(web_user):
+    def __init__(self, uName, password, fullName, address, telNum, email, starCard, points):
+        super(member, self).__init__(uName,password,fullName,address,telNum,email,starCard, [self])
+        self.m_points = points
+        self.type = 'C'
+
+class basic_empoyee(web_user):
+    def __init__(self, uName, password, fullName, address, telNum, email, starCard, membersList):
+        super(empoyee, self).__init__(uName, password, fullName, address, telNum, email, starCard, [self] + membersList)
+        self.type = 'E'
+
+class manager(basic_empoyee):
+    def __init__(self, uName, password, fullName, address, telNum, email, starCard, webUserList):
+        super(manager, self).__init__(self, uName, password, fullName, address, telNum, email, starCard, webUserList)
+        self.type = 'M'
