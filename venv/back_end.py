eb9154d1e60dc42595a7718bbc43ebe50120391e
AhsanAAR <39828020+AhsanAAR@users.noreply.github.com>
2020-04-11 15:58:46 +0500

    back_end

---
+++ venv/back_end.py
@@ -0,0 +1,15 @@
+import app
+
+class star_card:
+    def __init__(self, cardNum, ):
+
+
+class web_user:
+    def __init__(self, uName, password, fullName, address, telNum, email, starCard):
+        self.m_uName = uName
+        self.m_password = password
+        self.m_fullName = fullName
+        self.m_address = address
+        self.m_telNum = telNum
+        self.m_email = email
+        self.m_starCard = starCard
